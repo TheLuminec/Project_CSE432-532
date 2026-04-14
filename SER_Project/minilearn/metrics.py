1dfbe77f0ec19f094768fe719d956c453f90b6a3
theluminec <theluminec@gmail.com>
2026-04-14 18:28:05 -0400

    Metrics implementation

--- SER_Project/minilearn/metrics.py
+++ SER_Project/minilearn/metrics.py
@@ -1,14 +1,26 @@
+import numpy as np
+
 def accuracy_score(y_true, y_pred):
-    pass
+    return np.mean(y_true == y_pred)
 
 def precision_score(y_true, y_pred):
-    pass
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    return tp / (tp + fp)
 
 def recall_score(y_true, y_pred):
-    pass
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    return tp / (tp + fn)
 
 def f1_score(y_true, y_pred):
-    pass
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    return 2 * (precision * recall) / (precision + recall)
 
 def confusion_matrix(y_true, y_pred):
-    pass
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    tn = np.sum((y_true == 0) & (y_pred == 0))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    return np.array([[tn, fp], [fn, tp]])
